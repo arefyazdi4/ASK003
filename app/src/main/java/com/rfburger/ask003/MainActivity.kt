a1b2ee6f34e62ba5f6c4ed940d81f60e014d73b9
RF_BURGER <Arefyazdi4@gmail.com>
2021-03-14 02:37:21 +0330

    interactive setListener

--- app/src/main/java/com/rfburger/ask003/MainActivity.kt
+++ app/src/main/java/com/rfburger/ask003/MainActivity.kt
@@ -2,6 +2,9 @@ package com.rfburger.ask003
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,6 +12,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val zipcodeEditText :EditText = findViewById(R.id.zipCode_EditText)
+        val enterbutton : Button = findViewById(R.id.enterButton)
+
+        enterbutton.setOnClickListener {
+            val zipcode:String = zipcodeEditText.text.toString()
+
+            if(zipcode.length == 5){
+                Toast.makeText(this , zipcode , Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(this , R.string.errorMessage , Toast.LENGTH_SHORT).show()
+            }
+
+        }
     }
 
     override fun onStart() {
