69b76bd88f3f096dba9742ae49bf21d93b221ea1
RF_BURGER <Arefyazdi4@gmail.com>
2021-03-13 16:12:52 +0330

    layouts test xml

--- app/src/main/java/com/rfburger/ask003/MainActivity.kt
+++ app/src/main/java/com/rfburger/ask003/MainActivity.kt
@@ -4,8 +4,36 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    // region setup
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+    // endregion setup
+    // region teardown
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    // endregion teardown
+
+
 }
