f2c561d10a5a35bfb3b804513074e8e5a245d329
RF_BURGER <Arefyazdi4@gmail.com>
2021-03-16 12:21:18 +0330

    'Login_page'

---
+++ app/src/main/java/com/rfburger/ask003/LoaderActivity.kt
@@ -0,0 +1,18 @@
+package com.rfburger.ask003
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class LoaderActivity : AppCompatActivity() {
+
+    // region setup
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loader)
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/rfburger/ask003/LoginActivity.kt
@@ -0,0 +1,15 @@
+package com.rfburger.ask003
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class LoginActivity : AppCompatActivity() {
+
+    // region setup
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+    }
+}
