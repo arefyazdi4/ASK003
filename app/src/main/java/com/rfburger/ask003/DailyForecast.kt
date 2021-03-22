00207be06b479ae3c27d2c248326da6dba7b8379
RF_BURGER <Arefyazdi4@gmail.com>
2021-03-23 02:46:43 +0430

    recyclerView

---
+++ app/src/main/java/com/rfburger/ask003/DailyForecast.kt
@@ -0,0 +1,6 @@
+package com.rfburger.ask003
+
+data class DailyForecast(
+        val temp: Float,
+        val description: String
+)
--- app/src/main/java/com/rfburger/ask003/MainActivity.kt
+++ app/src/main/java/com/rfburger/ask003/MainActivity.kt
@@ -5,52 +5,78 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    val forecastRepository = ForecastRepository()
     // region setup
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val zipcodeEditText :EditText = findViewById(R.id.zipCode_EditText)
-        val enterbutton : Button = findViewById(R.id.enterButton)
+        val zipcodeEditText: EditText = findViewById(R.id.zipCode_EditText)
+        val enterButton: Button = findViewById(R.id.enterButton)
 
-        enterbutton.setOnClickListener {
-            val zipcode:String = zipcodeEditText.text.toString()
+        enterButton.setOnClickListener {
+            val zipcode: String = zipcodeEditText.text.toString()
 
-            if(zipcode.length == 5){
-                Toast.makeText(this , zipcode , Toast.LENGTH_SHORT).show()
-            }else{
-                Toast.makeText(this , R.string.errorMessage , Toast.LENGTH_SHORT).show()
+            if (zipcode.length == 5) {
+                forecastRepository.loadForecast(zipcode)
+            } else {
+                Toast.makeText(this, R.string.errorMessage, Toast.LENGTH_SHORT).show()
             }
 
         }
+
+        val forecastList : RecyclerView = findViewById(R.id.forecastList)
+        forecastList.layoutManager = LinearLayoutManager(this)
+        val dailyForecastAdapter = DailyForecastAdapter()
+        forecastList.adapter = dailyForecastAdapter
+
+
+        val weeklyForecastObserver = Observer<List<DailyForecast>>{ forecastItems ->
+            //Update our list adapter
+                dailyForecastAdapter.submitList(forecastItems)
+        }
+        forecastRepository.weeklyForecast.observe(this , weeklyForecastObserver)
+
     }
-
-    override fun onStart() {
-        super.onStart()
-    }
-
-    override fun onResume() {
-        super.onResume()
-    }
-    // endregion setup
-    // region teardown
-
-    override fun onPause() {
-        super.onPause()
-    }
-
-    override fun onStop() {
-        super.onStop()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-    }
-
-    // endregion teardown
-
 
 }
+
+
+
+
+
+
+
+
+//
+//    override fun onStart() {
+//        super.onStart()
+//    }
+//
+//    override fun onResume() {
+//        super.onResume()
+//    }
+//    // endregion setup
+//    // region teardown
+//
+//    override fun onPause() {
+//        super.onPause()
+//    }
+//
+//    override fun onStop() {
+//        super.onStop()
+//    }
+//
+//    override fun onDestroy() {
+//        super.onDestroy()
+//    }
+//
+//    // endregion teardown
+
